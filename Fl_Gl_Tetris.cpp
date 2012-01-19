4f447f268566935da5906461f63152cc57ac0367
Alan Jacobs Richardson <ajrich@earthlink.net>
2012-01-19 06:27:25 -0800

    Fl_Gl_Tetris.cpp: Implement DataDoubleBuffer, expand constructor initialization list, naive implementation of setGridMode & setColorMode, draw stub.
    Fl_Gl_Tetrix.hpp: Define helper objects DataBuffer and DataDoubleBuffer. Eliminate throw specification. Declare private data members.
    
    Makefile.am: Compile and link TetrisGame object into tetris_fltk.

--- Fl_Gl_Tetris.cpp
+++ Fl_Gl_Tetris.cpp
@@ -1,28 +1,69 @@
 #include "Fl_Gl_Tetris.hpp"
- 
+
+#define WBUFF swapped ? 1:0;
+#define RBUFF swapped ? 0:1;
+
+// DataDoubleBuffer
+
+void DataDoubleBuffer::write(const Field &field_, const Piece &cur, const Piece *holdp)
+{
+  std::lock_guard<std::mutex> lg(swap_guard);
+  auto idx = WBUFF;
+
+  buffers[idx].field=field_;
+  buffers[idx].current=cur;
+  if(nullptr!=holdp)
+    {
+      buffers[idx].hold=*holdp;
+      buffers[idx].holding=true;
+    }
+  else
+    {
+      buffers[idx].holding=false;
+    }
+}
+
+const DataBuffer& DataDoubleBuffer::swap_and_read()
+{
+  swap_guard.lock();
+  swapped = !swapped;
+  swap_guard.unlock();
+  auto idx = RBUFF;
+  return buffers[idx];
+}
+
+// Fl_Gl_Tetris
 
 Fl_Gl_Tetris::Fl_Gl_Tetris( int x,int y,int w,int h, const char *l):
-  Fl_Gl_Window(x,y,w,h,l)
+  Fl_Gl_Window(x,y,w,h,l),
+  gmod(0),cmod(0),
+  mBuff(),mCB(&mBuff,&DataDoubleBuffer::write),mGame(&mCB)
 {
 }
-Fl_Gl_Tetris::~Fl_Gl_Tetris() throw()
+Fl_Gl_Tetris::~Fl_Gl_Tetris()
 {
 }
 
 // Callbacks named in tetris_fltkgui.fl
 void Fl_Gl_Tetris::setGridMode(char mode)
 {
-  // STUB
+  gmod=mode;
 }
 void Fl_Gl_Tetris::setColorMode(char mode)
 {
-  // STUB
+  cmod=mode;
 }
 void Fl_Gl_Tetris::startTetris()
 {
   // STUB
 }
 void Fl_Gl_Tetris::reset()
+{
+  // STUB
+}
+
+//
+void Fl_Gl_Tetris::draw()
 {
   // STUB
 }
--- Fl_Gl_Tetris.hpp
+++ Fl_Gl_Tetris.hpp
@@ -1,24 +1,58 @@
 #ifndef FL_GL_TETRIS_HPP
 #define FL_GL_TETRIS_HPP
 
+#include <mutex>
+
 #include <FL/Fl.H>
 #include <FL/Fl_Gl_Window.H>
+#include "TetrisGame.hpp"
+#include "Field.hpp"
+#include "Piece.hpp"
+
+struct DataBuffer
+{
+  Field field;
+  Piece current,hold;
+  bool holding;
+  DataBuffer():current(I,0,nullptr),hold(I,0,nullptr),holding(false)
+  {}
+};
+class DataDoubleBuffer
+{
+  DataBuffer buffers[2];
+  bool swapped;
+  std::mutex swap_guard;
+public:
+  DataDoubleBuffer():swapped(false),swap_guard()
+  {}
+
+  void write(const Field &, const Piece &, const Piece *);
+  const DataBuffer& swap_and_read();
+};
 
 class Fl_Gl_Tetris: public Fl_Gl_Window
 {
 public:
   
   Fl_Gl_Tetris(int x,int y,int h ,int w, const char *l=0);
-  virtual ~Fl_Gl_Tetris() throw();
+  virtual ~Fl_Gl_Tetris();
   
   // Callbacks named in tetris_fltkgui.fl
   void setGridMode(char mode);
   void setColorMode(char mode);
   void startTetris();
   void reset();
+
+  //
+  void draw();
 protected:
 private:
-  
+  char gmod,cmod;
+
+  DataDoubleBuffer mBuff;
+  RenderFunc<DataDoubleBuffer> mCB;
+  TetrisGame mGame;
+
 };
 
 
