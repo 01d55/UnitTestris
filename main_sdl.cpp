74b5dff7246a05ca6ff3908d23a3eb0b1744209a
Alan Jacobs Richardson <ajrich@earthlink.net>
2012-02-07 01:06:04 -0800

    configure.ac: tetris_sdl requires gl package info.
    main_sdl.cpp: Some more GL init code.

--- main_sdl.cpp
+++ main_sdl.cpp
@@ -1,6 +1,14 @@
 #include <SDL.h>
+#include <GL/gl.h>
 
 #include <iostream>
+
+
+constexpr unsigned WIDTH=200,HEIGHT=400;
+
+
+bool init_gl();
+bool init_sdl_context();
 
 int main()
 {
@@ -12,13 +20,63 @@ int main()
       return retcode;
     }
 
+  if(!init_sdl_context)
+    {
+      SDL_Quit();
+      return -1;
+    }
+
+  if(!init_gl())
+    {
+      SDL_Quit();
+      return -1;
+    }
+
+
+  
+
+  SDL_Quit();
+
+  return 0;
+}
+
+bool init_sdl_context()
+{
+
+  const SDL_VideoInfo *infoptr;
+
+  infoptr = SDL_GetVideoInfo();
+
+  if(nullptr == infoptr)
+    {
+      std::cerr << "GetVideoInfo failed: " << SDL_GetError() << std::endl;
+      return false;
+    }
+
   SDL_GL_SetAttribute(SDL_GL_RED_SIZE,8);
   SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,8);
   SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,8);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,16);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
 
-  SDL_Quit();
+  if(nullptr == 
+     SDL_SetVideoMode(WIDTH, HEIGHT, infoptr->vfmt->BitsPerPixel, SDL_OPENGL) )
+    {
+      std::cerr << "SetVideoMode failed: " << SDL_GetError() << std::endl;
+      return false;
+    }
 
-  return 0;
+  return true;
+}
+
+bool init_gl()
+{
+  GLint retcode=-1;
+
+  glGetIntegerv(GL_MAJOR_VERSION,&retcode);  
+  std::cout << "GL version: " << retcode << '.';
+  glGetIntegerv(GL_MINOR_VERSION,&retcode);
+  std::cout << retcode << '\n';
+
+  return true;
 }
