0af2912755a248e8694be59a7b4b676f96d16eb9
Alan Jacobs Richardson <arrlaari@gmail.com>
2016-03-23 13:15:30 -0700

    Adjust constructor naming to resemble what I've seen of ::std structs

--- rust/UnitTestris/src/gamestate/field.rs
+++ rust/UnitTestris/src/gamestate/field.rs
@@ -1,7 +1,7 @@
 use std::result::Result;
 use super::common::Coord;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct Field;
 
 #[derive(Debug)]
@@ -28,7 +28,11 @@ impl FieldSpec for Vec<Vec<bool>> {
 #[allow(dead_code, unused_variables)]
 impl Field {
 
-    pub fn new<T:FieldSpec>(spec:&T) -> Field {
+    pub fn new() -> Field {
+        Field::default()
+    }
+
+    pub fn from_rectangular_vector(rect: & Vec<Vec<bool>>) -> Result<Field, SizeError> {
         unimplemented!()
     }
 
@@ -60,125 +64,123 @@ mod test {
     use super::super::common::FIELD_WIDTH;
     use super::super::common::Coord;
     #[test]
-    fn test_constructor() {
-        // Test constructing with the null type
+    fn test_new() {
+        let test_field = Field::new();
+        for j in 0..FIELD_HEIGHT {
+            for i in 0..FIELD_WIDTH {
+                assert_eq!(false, test_field.get(Coord::new(i as i32,j as i32)).unwrap())
+            }
+        }
+    }
+    #[test]
+    fn test_from_rectangular_vector() {
+        // test constructing with a two-dimensional vector
+        let mut test_input: Vec<Vec<bool> > = Vec::new();
+        // size, and set to all false
+        test_input.resize(FIELD_WIDTH, Vec::new());
+        for ref mut j in & mut test_input {
+            j.resize(FIELD_HEIGHT, false)
+        }
+        // empty field
         {
-            let test_field = Field::new::<()>(&());
+            let test_field = Field::from_rectangular_vector(&test_input.clone()).unwrap();
             for j in 0..FIELD_HEIGHT {
                 for i in 0..FIELD_WIDTH {
                     assert_eq!(false, test_field.get(Coord::new(i as i32,j as i32)).unwrap())
                 }
             }
         }
-        // test constructing with a two-dimensional vector
+        // check extreme corners
+        test_input[0][0] = true;
+        // Putting FIELD_HEIGHT-1 in a match pattern is a syntax error.
+        const FIELD_WEND: usize = FIELD_HEIGHT-1;
+        const FIELD_HEND: usize = FIELD_WIDTH-1;
+        test_input[FIELD_WEND][FIELD_HEND] = true;
         {
-            let mut test_input: Vec<Vec<bool> > = Vec::new();
-            // size, and set to all false
-            test_input.resize(FIELD_WIDTH, Vec::new());
-            for ref mut j in & mut test_input {
-                j.resize(FIELD_HEIGHT, false)
-            }
-            // empty field
-            {
-                let test_field = Field::new::<Vec<Vec<bool>>>(&test_input.clone());
-                for j in 0..FIELD_HEIGHT {
-                    for i in 0..FIELD_WIDTH {
-                        assert_eq!(false, test_field.get(Coord::new(i as i32,j as i32)).unwrap())
-                    }
-                }
-            }
-            // check extreme corners
-            test_input[0][0] = true;
-            // Putting FIELD_HEIGHT-1 in a match pattern is a syntax error.
-            const FIELD_WEND: usize = FIELD_HEIGHT-1;
-            const FIELD_HEND: usize = FIELD_WIDTH-1;
-            test_input[FIELD_WEND][FIELD_HEND] = true;
-            {
-                let test_field = Field::new(&test_input.clone());
-                for j in 0..FIELD_HEIGHT {
-                    for i in 0..FIELD_WIDTH {
-                        match (i,j) {
-                            (0,0) | (FIELD_WEND, FIELD_HEND) => { assert!(test_field.get(Coord::new(i as i32, j as i32)).unwrap()) }
-                            _ => { assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap()) }
-                        }
-                    }
-                }
-            }
-            // a non-full row placed over a full row. The non-full row should drop into the space opened when the full row is cleared.
+            let test_field = Field::from_rectangular_vector(&test_input.clone()).unwrap();
             for j in 0..FIELD_HEIGHT {
                 for i in 0..FIELD_WIDTH {
                     match (i,j) {
-                        (_,0) => {test_input[i][j] = true}
-                        (_,1) => {test_input[i][j] = i%2 == 0}
-                        _ => {test_input[i][j] = false}
+                        (0,0) | (FIELD_WEND, FIELD_HEND) => { assert!(test_field.get(Coord::new(i as i32, j as i32)).unwrap()) }
+                        _ => { assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap()) }
                     }
                 }
             }
-            {
-                let test_field = Field::new(&test_input.clone());
-                for j in 0..FIELD_HEIGHT {
-                    for i in 0..FIELD_WIDTH {
-                        match (i,j) {
-                            (_,0) => { assert_eq!(i%2 == 0, test_field.get(Coord::new(i as i32, j as i32)).unwrap()) }
-                            _ => { assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap())}
-                        }
-                    }
+        }
+        // a non-full row placed over a full row. The non-full row should drop into the space opened when the full row is cleared.
+        for j in 0..FIELD_HEIGHT {
+            for i in 0..FIELD_WIDTH {
+                match (i,j) {
+                    (_,0) => {test_input[i][j] = true}
+                    (_,1) => {test_input[i][j] = i%2 == 0}
+                    _ => {test_input[i][j] = false}
                 }
             }
-            // Clear multiple rows.
-            // Three full rows, two contiguous, one non contiguous,
-            // and a row with alternating empty and full blocks above.
-            // The row with alternating blocks should shift down 3 as the full
-            // rows beneath it are removed.
+        }
+        {
+            let test_field = Field::from_rectangular_vector(&test_input.clone()).unwrap();
             for j in 0..FIELD_HEIGHT {
+                for i in 0..FIELD_WIDTH {
+                    match (i,j) {
+                        (_,0) => { assert_eq!(i%2 == 0, test_field.get(Coord::new(i as i32, j as i32)).unwrap()) }
+                        _ => { assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap())}
+                    }
+                }
+            }
+        }
+        // Clear multiple rows.
+        // Three full rows, two contiguous, one non contiguous,
+        // and a row with alternating empty and full blocks above.
+        // The row with alternating blocks should shift down 3 as the full
+        // rows beneath it are removed.
+        for j in 0..FIELD_HEIGHT {
+            for i in 0..FIELD_WIDTH {
+                match(i,j) {
+                    (_,1) | (_,2) | (_,5) => { test_input[i][j] = true }
+                    (_,6) => { test_input[i][j] = i%2 != 0 }
+                    _ => { test_input[i][j] = false }
+                }
+            }
+        }
+        {
+            let test_field = Field::from_rectangular_vector(&test_input.clone()).unwrap();
+            for j in 0 ..FIELD_HEIGHT {
                 for i in 0..FIELD_WIDTH {
                     match(i,j) {
-                        (_,1) | (_,2) | (_,5) => { test_input[i][j] = true }
-                        (_,6) => { test_input[i][j] = i%2 != 0 }
-                        _ => { test_input[i][j] = false }
+                        (_,3) => {assert_eq!(i%2 != 0, test_field.get(Coord::new(i as i32, j as i32)).unwrap())}
+                        _ => {assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap())}
                     }
                 }
             }
-            {
-                let test_field = Field::new(&test_input.clone());
-                for j in 0 ..FIELD_HEIGHT {
-                    for i in 0..FIELD_WIDTH {
-                        match(i,j) {
-                            (_,3) => {assert_eq!(i%2 != 0, test_field.get(Coord::new(i as i32, j as i32)).unwrap())}
-                            _ => {assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap())}
-                        }
-                    }
-                }
+        }
+        // Full column.
+        for j in 0..FIELD_HEIGHT {
+            for i in 0..FIELD_WIDTH {
+                test_input[i][j] = i == 0
             }
-            // Full column.
+        }
+        {
+            let test_field = Field::from_rectangular_vector(&test_input.clone()).unwrap();
             for j in 0..FIELD_HEIGHT {
                 for i in 0..FIELD_WIDTH {
-                    test_input[i][j] = i == 0
+                    assert_eq!( i == 0, test_field.get(Coord::new(i as i32, j as i32)).unwrap())
                 }
             }
-            {
-                let test_field = Field::new(&test_input.clone());
-                for j in 0..FIELD_HEIGHT {
-                    for i in 0..FIELD_WIDTH {
-                        assert_eq!( i == 0, test_field.get(Coord::new(i as i32, j as i32)).unwrap())
-                    }
-                }
-            }
-            // All rows full. The full rows should clear.
-            for ref mut x in  &mut test_input {
-                x.clear();
-                x.resize(FIELD_HEIGHT, true)
-            }
-            {
-                let test_field = Field::new(&test_input.clone());
-                for j in 0..FIELD_HEIGHT {
-                    for i in 0..FIELD_WIDTH {
-                        assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap())
-                    }
-                }
-            }
-            // TODO: c++ throws FieldSizeError from constructor
         }
+        // All rows full. The full rows should clear.
+        for ref mut x in  &mut test_input {
+            x.clear();
+            x.resize(FIELD_HEIGHT, true)
+        }
+        {
+            let test_field = Field::from_rectangular_vector(&test_input.clone()).unwrap();
+            for j in 0..FIELD_HEIGHT {
+                for i in 0..FIELD_WIDTH {
+                    assert_eq!(false, test_field.get(Coord::new(i as i32, j as i32)).unwrap())
+                }
+            }
+        }
+        // TODO: c++ throws FieldSizeError from constructor
     }
     #[test]
     fn test_set() {
